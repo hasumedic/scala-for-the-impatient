b1710dbf1f3e7907c0bed2f5bca0aeb511074f1b
Alex Salguero <hasumedic@gmail.com>
2015-12-03 00:45:42 +0000

    Chapter 3 exercises

---
+++ src/main/scala/Chapter3.scala
@@ -0,0 +1,97 @@
+import java.awt.datatransfer.{DataFlavor, SystemFlavorMap}
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.JavaConversions.asScalaBuffer
+
+object Chapter3 {
+
+  //Exercise 1
+  // Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n (exclusive)
+  def exercise1(size: Int): Array[Int] = {
+    val a = for (i <- 0 until size) yield util.Random.nextInt(size)
+    a.toArray
+  }
+
+  //Exercise 2
+  // Write a loop that swaps adjacent elements of an array of integers. For example, Array(1, 2, 3, 4, 5) becomes
+  // Array(2, 1, 4, 3, 5)
+  def exercise2(array: Array[Int]): Array[Int] = {
+    for (i <- array.indices if i % 2 != 0) {
+      val temp = array(i)
+      array(i) = array(i - 1)
+      array(i - 1) = temp
+    }
+    array
+  }
+
+  //Exercise 3
+  // Repeat the preceding assignment, but produce a new array with the swapped values. Use for/yield
+  def exercise3(array: Array[Int]): Array[Int] = {
+    (for (i <- array.indices) yield {
+      if (i % 2 == 0 && i == array.length - 1) array(i)
+      else if (i % 2 == 0) array(i + 1)
+      else array(i - 1)
+    }).toArray
+  }
+
+  //Exercise 4
+  // Given an array of integers, produce a new array that contains all positive values of the original array, in their
+  // original order, followed by all values that are zero or negative in their original order
+  def exercise4(array: Array[Int]): Array[Int] = {
+    array.filter(_ > 0) ++ array.filter(_ <= 0)
+  }
+
+  //Exercise 5
+  // How do you compute the average of an Array[Double]?
+  def exercise5(array: Array[Double]): Double = {
+    if (array.isEmpty) 0
+    else array.sum / array.length
+  }
+
+  //Exercise 6
+  // How do you rearrange the elements of an Array[Int] so that they appear in reverse sorted order?
+  // How do you do the same with an ArrayBuffer[Int]?
+  def exercise6Array(array: Array[Int]): Array[Int] = array.sorted.reverse
+
+  def exercise6ArrayBuffer(buffer: ArrayBuffer[Int]): ArrayBuffer[Int] = buffer.sorted.reverse
+
+  //Exercise 7
+  // Write a code snippet that produces all values from an array with duplicates removed
+  def exercise7[T](array: Array[T]): Array[T] = array.distinct
+
+  //Exercise 8
+  // Rewrite the example at the end of Section 3.4, "Transforming Arrays". Collect indexes of the negative elements,
+  // reverse the sequence, drop the last index, and call a.remove(i) for each index.
+  def exercise8(array: ArrayBuffer[Int]): ArrayBuffer[Int] = {
+    val indexes = for (i <- array.indices if array(i) < 0) yield i
+    val indexesLessFirstNegative = indexes.reverse.dropRight(1)
+    for (j <- indexesLessFirstNegative.indices) {
+      array.remove(indexesLessFirstNegative(j))
+    }
+    array
+  }
+
+  //Exercise 9
+  // Make a collection of all time zones returned by java.util.Timezone.getAvailableIDs that are in America.
+  // Strip off the "America/" prefix and sort the result
+  def exercise9(): Array[String] = {
+    val prefixLookup = "America/"
+    val timezones = java.util.TimeZone.getAvailableIDs
+    (for (timezone <- timezones if timezone.startsWith(prefixLookup)) yield {
+      timezone.stripPrefix(prefixLookup)
+    }).sorted
+  }
+
+  //Exercise 10
+  // Import java.awt.datatransfer._ and make an object of type SystemFlavorMap with the call
+  // val flavors = SystemFlavorMap.getDefaultFlavorMap().asInstanceOf[SystemFlavorMap]
+  // Then call the getNativesForFlavor method with parameter DataFlavor.imageFlavor and get the return value as a
+  // Scala buffer
+  def exercise10(): mutable.Buffer[String] = {
+    val flavors = SystemFlavorMap.getDefaultFlavorMap.asInstanceOf[SystemFlavorMap]
+    val natives: mutable.Buffer[String] = flavors.getNativesForFlavor(DataFlavor.imageFlavor)
+    natives
+  }
+}
+
---
+++ src/test/scala/Chapter3Test.scala
@@ -0,0 +1,116 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+import scala.collection.mutable.ArrayBuffer
+
+class Chapter3Test extends FlatSpec with Matchers {
+
+  "exercise 1" should "create an array of size n with elements between 0 and n-1" in {
+    val array1 = Chapter3.exercise1(3)
+    array1.length should be(3)
+    array1.forall(element => element >= 0 && element < 3)
+
+    val array2 = Chapter3.exercise1(10)
+    array2.length should be(10)
+    array1.forall(element => element >= 0 && element < 10)
+  }
+
+  "exercise 2" should "swap nothing on empty input" in {
+    Chapter3.exercise2(Array()) should be(Array())
+  }
+  "exercise 2" should "swap nothing on arrays with a single element" in {
+    Chapter3.exercise2(Array(1)) should be(Array(1))
+  }
+  "exercise 2" should "swap the elements on an array with 2 elements" in {
+    Chapter3.exercise2(Array(1, 2)) should be(Array(2, 1))
+  }
+  "exercise 2" should "swap the elements on an array with multiple elements" in {
+    Chapter3.exercise2(Array(1, 2, 3, 4, 5)) should be(Array(2, 1, 4, 3, 5))
+  }
+
+  // Exercise 3 is the same as 2 but is looking for a slightly different implementation
+  "exercise 3" should "swap nothing on empty input" in {
+    Chapter3.exercise3(Array()) should be(Array())
+  }
+  "exercise 3" should "swap nothing on arrays with a single element" in {
+    Chapter3.exercise3(Array(1)) should be(Array(1))
+  }
+  "exercise 3" should "swap the elements on an array with 2 elements" in {
+    Chapter3.exercise3(Array(1, 2)) should be(Array(2, 1))
+  }
+  "exercise 3" should "swap the elements on an array with multiple elements" in {
+    Chapter3.exercise3(Array(1, 2, 3, 4, 5)) should be(Array(2, 1, 4, 3, 5))
+  }
+
+  "exercise 4" should "reorder nothing given an empty array" in {
+    Chapter3.exercise4(Array()) should be(Array())
+  }
+  "exercise 4" should "reorder nothing given an array with a single element" in {
+    Chapter3.exercise4(Array(-10)) should be(Array(-10))
+  }
+  "exercise 4" should "reorder given an array with a couple of elements" in {
+    Chapter3.exercise4(Array(-10, 10)) should be(Array(10, -10))
+  }
+  "exercise 4" should "reorder given an array with multiple elements" in {
+    Chapter3.exercise4(Array(-10, 10, 5, -5)) should be(Array(10, 5, -10, -5))
+  }
+  "exercise 4" should "reorder given an array with multiple elements including zeros" in {
+    Chapter3.exercise4(Array(-10, 0, 10, 5, -5)) should be(Array(10, 5, -10, 0, -5))
+  }
+
+  "exercise 5" should "return 0 as average for an empty double array" in {
+    Chapter3.exercise5(Array()) should be(0)
+  }
+  "exercise 5" should "return the element contained in a single element array as its average" in {
+    Chapter3.exercise5(Array(5)) should be(5)
+  }
+  "exercise 5" should "return the average of an array with two elements" in {
+    Chapter3.exercise5(Array(0, 5)) should be(2.5)
+  }
+  "exercise 5" should "return the average of an array containing negative elements" in {
+    Chapter3.exercise5(Array(-5, 5)) should be(0)
+  }
+  "exercise 5" should "return the average of multiple elements in disorder and containing negative elements" in {
+    Chapter3.exercise5(Array(8, 2, -4, 0, 4, -1)) should be(1.5)
+  }
+
+  "exercise 6 Array" should "should resolve base cases" in {
+    Chapter3.exercise6Array(Array()) should be(Array())
+    Chapter3.exercise6Array(Array(1)) should be(Array(1))
+    Chapter3.exercise6Array(Array(-1)) should be(Array(-1))
+  }
+  "exercise 6 Array" should "reverse order a short ordered array" in {
+    Chapter3.exercise6Array(Array(1, 2, 3, 4)) should be(Array(4, 3, 2, 1))
+  }
+  "exercise 6 Array" should "reverse order a short unordered array" in {
+    Chapter3.exercise6Array(Array(3, 2, 4, 1)) should be(Array(4, 3, 2, 1))
+  }
+  "exercise 6 Array" should "reverse order a long unordered array with negatives" in {
+    Chapter3.exercise6Array(Array(-1, 4, 3, -10, 2, 0)) should be(Array(4, 3, 2, 0, -1, -10))
+  }
+  "exercise 6 ArrayBuffer" should "should resolve base cases" in {
+    Chapter3.exercise6ArrayBuffer(ArrayBuffer()) should be(ArrayBuffer())
+    Chapter3.exercise6ArrayBuffer(ArrayBuffer(1)) should be(ArrayBuffer(1))
+    Chapter3.exercise6ArrayBuffer(ArrayBuffer(-1)) should be(ArrayBuffer(-1))
+  }
+  "exercise 6 ArrayBuffer" should "reverse order a short ordered array" in {
+    Chapter3.exercise6ArrayBuffer(ArrayBuffer(1, 2, 3, 4)) should be(ArrayBuffer(4, 3, 2, 1))
+  }
+  "exercise 6 ArrayBuffer" should "reverse order a short unordered array" in {
+    Chapter3.exercise6ArrayBuffer(ArrayBuffer(3, 2, 4, 1)) should be(ArrayBuffer(4, 3, 2, 1))
+  }
+  "exercise 6 ArrayBuffer" should "reverse order a long unordered array with negatives" in {
+    Chapter3.exercise6ArrayBuffer(ArrayBuffer(-1, 4, 3, -10, 2, 0)) should be(ArrayBuffer(4, 3, 2, 0, -1, -10))
+  }
+
+  "exercise 7" should "remove duplicates from any given array" in {
+    Chapter3.exercise7(Array()) should be(Array())
+    Chapter3.exercise7(Array(1, 1)) should be(Array(1))
+    Chapter3.exercise7(Array(1, 1, 3, 4, 4)) should be(Array(1, 3, 4))
+    Chapter3.exercise7(Array('a', 'b', 'b', 'c')) should be(Array('a', 'b', 'c'))
+    Chapter3.exercise7(Array(true, true, false)) should be(Array(true, false))
+  }
+
+  "exercise 8" should "remove all negative numbers but first occurrence" in {
+    Chapter3.exercise8(ArrayBuffer(1, 5, 6, -2, 4, 5, -2, -4, -3)) should be(ArrayBuffer(1, 5, 6, -2, 4, 5))
+  }
+}

a12d2e07d3346e431b300fd7f591d22d7b918b86
Alex Salguero <hasumedic@gmail.com>
2015-12-06 21:53:48 +0000

    Chapter 4 exercises

---
+++ src/main/scala/Chapter4.scala
@@ -0,0 +1,97 @@
+import java.util.{Calendar, Scanner}
+
+import scala.collection.JavaConversions.mapAsScalaMap
+import scala.collection.{SortedMap, mutable}
+
+object Chapter4 {
+
+  def exercise1(gizmos: Map[String, Double]): Map[String, Double] = {
+    def tenPercentDiscount(value: Double): Double = {
+      math.round(value - value * 10 / 100)
+    }
+
+    for ((k, v) <- gizmos) yield (k, tenPercentDiscount(v))
+  }
+
+  def exercise2(filename: String): mutable.Map[String, Int] = {
+    var map = mutable.Map[String, Int]()
+    var word: String = ""
+    val in = new Scanner(new java.io.File(getClass.getResource(filename).getFile))
+    while (in.hasNext) {
+      word = in.next()
+      map += (word -> (map.getOrElse(word, 0) + 1))
+    }
+    map
+  }
+
+  def exercise3(filename: String): Map[String, Int] = {
+    var map = Map[String, Int]()
+    var word: String = ""
+    val in = new Scanner(new java.io.File(getClass.getResource(filename).getFile))
+    while (in.hasNext) {
+      word = in.next()
+      map = map + (word -> (map.getOrElse(word, 0) + 1))
+    }
+    map
+  }
+
+  def exercise4(filename: String): SortedMap[String, Int] = {
+    var map = SortedMap[String, Int]()
+    var word: String = ""
+    val in = new Scanner(new java.io.File(getClass.getResource(filename).getFile))
+    while (in.hasNext) {
+      word = in.next()
+      map = map + (word -> (map.getOrElse(word, 0) + 1))
+    }
+    map foreach println
+    map
+  }
+
+  //Exercise 5: Haven't bothered with Java backwards compat on TreeMaps
+
+  def exercise6(): mutable.LinkedHashMap[String, Int] = {
+    mutable.LinkedHashMap(
+      "Monday" -> Calendar.MONDAY,
+      "Tuesday" -> Calendar.TUESDAY,
+      "Wednesday" -> Calendar.WEDNESDAY,
+      "Thursday" -> Calendar.THURSDAY,
+      "Friday" -> Calendar.FRIDAY,
+      "Saturday" -> Calendar.SATURDAY,
+      "Sunday" -> Calendar.SUNDAY
+    )
+  }
+
+
+  def exercise7(): Unit = {
+
+    def longestPropertyLenght: Int = {
+      val properties = System.getProperties
+
+      var max = 0
+      val it = properties.keys()
+      while (it.hasMoreElements) {
+        max = math.max(max, it.nextElement.toString.length)
+      }
+      max
+    }
+
+    def printProperty(p: (AnyRef, AnyRef)): Unit = {
+      println(p._1.toString.padTo(longestPropertyLenght, " ").mkString + " | " + p._2.toString)
+    }
+
+    System.getProperties.foreach(p => printProperty(p))
+  }
+
+  def exercise8(input: Array[Int]): (Int, Int) = {
+    if (input.isEmpty) throw new IllegalArgumentException
+    else (input.min, input.max)
+  }
+
+  def exercise9(input: Array[Int], v: Int): (Int, Int, Int) = {
+    if (input.isEmpty) throw new IllegalArgumentException
+    else (input.count(_ < v), input.count(_ == v), input.count(_ > v))
+  }
+
+  //Exercise 10
+  // In cryptography algorithms it might make sense to map the characters in the sequence with a particular key
+}
---
+++ src/test/scala/Chapter4Test.scala
@@ -0,0 +1,68 @@
+import java.util.Calendar
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.SortedMap
+
+class Chapter4Test extends FlatSpec with Matchers {
+
+  "exercise 1" should "provide a 10% discount on a given map of prices" in {
+    val gizmos = Map("phone" -> 99.99, "watch" -> 89.99, "gps" -> 149.49)
+    Chapter4.exercise1(gizmos) should be(Map("phone" -> 90.00, "watch" -> 81.00, "gps" -> 135.00))
+  }
+
+  "exercise 2" should "count the words for the given text file" in {
+    Chapter4.exercise2("Chapter4-2.txt") should be(Map("This" -> 2, "is" -> 2, "just" -> 2, "a" -> 2, "test." -> 2))
+  }
+
+  "exercise 3" should "count the words for the given text file" in {
+    Chapter4.exercise3("Chapter4-3.txt") should be(Map("This" -> 2, "is" -> 2, "just" -> 1, "a" -> 2, "test." -> 2))
+  }
+
+  "exercise 4" should "count the words for the given text file and print them in order" in {
+    Chapter4.exercise3("Chapter4-4.txt") should be(SortedMap("A" -> 1, "a" -> 2, "is" -> 2, "just" -> 1, "test" -> 1, "test." -> 2, "This" -> 1))
+  }
+
+  "exercise 6" should "create a LinkedHashMap for the days of the week in order" in {
+    var loopedMap: List[Int] = Nil
+    for ((k, v) <- Chapter4.exercise6()) {
+      loopedMap = loopedMap :+ v
+    }
+    loopedMap should contain theSameElementsInOrderAs List(Calendar.MONDAY, Calendar.TUESDAY, Calendar.WEDNESDAY, Calendar.THURSDAY, Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY)
+  }
+
+  "exercise 8" should "throw an exception given an empty array" in {
+    intercept[IllegalArgumentException] {
+      Chapter4.exercise8(Array())
+    }
+  }
+  "exercise 8" should "calculate min and max for a given array base cases" in {
+    Chapter4.exercise8(Array(1)) should be((1, 1))
+    Chapter4.exercise8(Array(1, 1, 1)) should be((1, 1))
+    Chapter4.exercise8(Array(1, 2)) should be((1, 2))
+    Chapter4.exercise8(Array(2, 1)) should be((1, 2))
+  }
+  "exercise 8" should "calculate min and max for a given array with repeated values" in {
+    Chapter4.exercise8(Array(1, 1, 2, 2, 3, 3)) should be(1, 3)
+  }
+  "exercise 8" should "calculate min and max for a given array with negatives" in {
+    Chapter4.exercise8(Array(1, -1, 2, -2, 3, -3)) should be(-3, 3)
+  }
+
+  "exercise 9" should "throw an exception given an empty array" in {
+    intercept[IllegalArgumentException] {
+      Chapter4.exercise9(Array(), 10)
+    }
+  }
+  "exercise 9" should "return the correct counts for base cases" in {
+    Chapter4.exercise9(Array(1), 5) should be((1, 0, 0))
+    Chapter4.exercise9(Array(1), 1) should be((0, 1, 0))
+    Chapter4.exercise9(Array(1), 0) should be((0, 0, 1))
+  }
+  "exercise 9" should "return the correct counts given larger arrays" in {
+    Chapter4.exercise9(Array(1, 2, 3, 4, 5, 6, 7, 8, 9), 5) should be((4, 1, 4))
+  }
+  "exercise 9" should "return the correct counts given negative numbers" in {
+    Chapter4.exercise9(Array(-1, 2, 0, -3, 4, -5, 0, -6, 7, 8, -9), 0) should be((5, 2, 4))
+  }
+}

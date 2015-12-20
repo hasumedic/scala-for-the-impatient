6a78292b6bfe61dbc33d54a5e17c3c6a247e77cb
Alex Salguero <hasumedic@gmail.com>
2015-12-20 13:32:47 +0000

    Chapter 6 exercises

---
+++ src/main/scala/Chapter6.scala
@@ -0,0 +1,95 @@
+import PlayingCardSuits.PlayingCardSuit
+
+import scala.math.BigDecimal.RoundingMode
+
+object Chapter6
+
+//Exercise 1
+object Conversions {
+
+  private val inchesToCentimetersRatio = 0.39370
+  private val gallonsToLitersRatio = 4.54609
+  private val milesToKilometersRatio = 0.62137
+
+  private def roundUp(number: Double): Double = {
+    BigDecimal(number).setScale(2, RoundingMode.HALF_UP).toDouble
+  }
+
+  def inchesToCentimeters(inches: Double): Double = roundUp(inches / inchesToCentimetersRatio)
+
+  def gallonsToLiters(gallons: Double): Double = roundUp(gallons * gallonsToLitersRatio)
+
+  def milesToKilometers(miles: Double): Double = roundUp(miles / milesToKilometersRatio)
+}
+
+
+//Exercise 2
+abstract class UnitConversion {
+  def convert(unit: Double): Double
+
+  def roundUp(number: Double): Double = {
+    BigDecimal(number).setScale(2, RoundingMode.HALF_UP).toDouble
+  }
+}
+
+object InchesToCentimeters extends UnitConversion {
+  private val ratio = 0.39370
+
+  override def convert(unit: Double): Double = roundUp(unit / ratio)
+}
+
+object GallonsToLiters extends UnitConversion {
+  private val ratio = 4.54609
+
+  override def convert(unit: Double): Double = roundUp(unit * ratio)
+}
+
+object MilesToKilometers extends UnitConversion {
+  private val ratio = 0.62137
+
+  override def convert(unit: Double): Double = roundUp(unit / ratio)
+}
+
+//Exercise 3
+object Origin extends java.awt.Point
+
+//This looks like a bad idea. Creating a singleton extending a mutable object...
+
+//Exercise 4
+class Point(val x: Int, val y: Int)
+
+object Point {
+  def apply(x: Int, y: Int): Point = new Point(x, y)
+}
+
+//Exercise 5
+object ReverseArgs extends App {
+  print(args.reverse.mkString(" "))
+}
+
+//Exercise 6
+object PlayingCardSuits extends Enumeration {
+  type PlayingCardSuit = Value
+
+  val Club = Value("\u2663")
+  val Diamond = Value("\u2666")
+  val Heart = Value("\u2666")
+  val Spade = Value("\u2660")
+}
+
+//Exercise 7
+def isRed(suit: PlayingCardSuit): Boolean = suit == PlayingCardSuits.Heart || suit == PlayingCardSuits.Diamond
+
+//Exercise 8
+object RGBColorCube extends Enumeration {
+  type RBGColor = Value
+
+  val Black = Value(0x000000)
+  val Red = Value(0xFF0000)
+  val Green = Value(0x00FF00)
+  val Blue = Value(0x0000FF)
+  val Cyan = Value(0x00FFFF)
+  val Magenta = Value(0xFF00FF)
+  val Yellow = Value(0xFFFF00)
+  val White = Value(0xFFFFFF)
+}
---
+++ src/test/scala/Chapter6Test.scala
@@ -0,0 +1,40 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+class Chapter6Test extends FlatSpec with Matchers {
+  "exercise 1" should "implement an object Conversions that converts inches to cm" in {
+    Conversions.inchesToCentimeters(0) should be(0)
+    Conversions.inchesToCentimeters(10) should be(25.4)
+    Conversions.inchesToCentimeters(14.3) should be(36.32)
+  }
+  "exercise 1" should "implement an object Conversions that converts gallons to liters" in {
+    Conversions.gallonsToLiters(0) should be(0)
+    Conversions.gallonsToLiters(4) should be(18.18)
+    Conversions.gallonsToLiters(3.24) should be(14.73)
+  }
+  "exercise 1" should "implement an object Conversions that converts miles to kilometers" in {
+    Conversions.milesToKilometers(0) should be(0)
+    Conversions.milesToKilometers(14) should be(22.53)
+    Conversions.milesToKilometers(21.11) should be(33.97)
+  }
+
+  "exercise 2" should "implement a super class UnitConversion with object InchesToCentimeters that implement it" in {
+    InchesToCentimeters.convert(0) should be(0)
+    InchesToCentimeters.convert(10) should be(25.4)
+    InchesToCentimeters.convert(14.3) should be(36.32)
+  }
+  "exercise 2" should "implement a super class UnitConversion with object GallonsToLiters that implement it" in {
+    GallonsToLiters.convert(0) should be(0)
+    GallonsToLiters.convert(4) should be(18.18)
+    GallonsToLiters.convert(3.24) should be(14.73)
+  }
+  "exercise 2" should "implement a super class UnitConversion with object MilesToKilometers that implement it" in {
+    MilesToKilometers.convert(0) should be(0)
+    MilesToKilometers.convert(14) should be(22.53)
+    MilesToKilometers.convert(21.11) should be(33.97)
+  }
+
+  "exercise 4" should "define a Point class which can be constructed as Point(3, 4)" in {
+    Point(3, 4).x should be (3)
+    Point(3, 4).y should be (4)
+  }
+}
